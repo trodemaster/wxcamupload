9400e8f462b95f287b3115dbbaca4c602b0fdbb1
Blake Garner <bgarner@adobe.com>
2017-09-09 22:00:02 -0700

    cull dead code

--- wxcamupload.go
+++ wxcamupload.go
@@ -1,18 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"github.com/dutchcoders/goftp"
-	//"io"
-	"log"
-	"net/http"
-	"os"
-	//"strings"
-	"bytes"
 	"github.com/oliamb/cutter"
 	"image"
 	"image/jpeg"
+	"log"
+	"net/http"
+	"os"
 	"reflect"
 )
 
@@ -80,9 +78,7 @@ func main() {
 	}
 	fmt.Println("img type:", reflect.TypeOf(img))
 
-	// the original turtle image is 2256 x 1504
 	// crop to just the upper left corner
-
 	cImg, err := cutter.Crop(img, cutter.Config{
 		Height:  800,               // height in pixel or Y ratio(see Ratio Option below)
 		Width:   1920,              // width in pixel or X ratio
@@ -95,7 +91,6 @@ func main() {
 		log.Fatal("Cannot crop image:", err)
 	}
 	fmt.Println("cImg dimension:", cImg.Bounds())
-	// Output: cImg dimension: (10,10)-(510,510)
 
 	// convert from image.Image to []byte
 	buf := &bytes.Buffer{}
@@ -103,36 +98,8 @@ func main() {
 		log.Fatalf("Error converting: %s\n", err)
 	}
 
-	// write []byte to file
-	// cropped_name := "cropped_file.jpg"
-	// out_file, out_err := os.Create(cropped_name)
-	// if out_err != nil {
-	// 	log.Fatalf("Error creating output file: %s\n", out_err)
-	// }
-	// if _, err := out_file.Write(buf.Bytes()); err != nil {
-	// 	log.Fatalf("Error writing to output file: %s\n", err)
-	// }
-	// fmt.Printf("Wrote %s\n", cropped_name)
-
-	// create a tempfile
-	//tempfile, err := os.Create("image.jpg")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer tempfile.Close()
-
-	// copy contents to file
-	//	filebuffer, err := io.Copy(tempfile, snap.Body)
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-
-	//	fmt.Println("filebuffer type:", reflect.TypeOf(filebuffer))
-
 	// write debug output
 	if wxcamuploadDebug {
-
-		//fmt.Println("File size: ", filebuffer)
 		fmt.Println("Source Url: ", camUrl)
 		fmt.Println("wunderground Url: ", wundergroundUrl)
 		fmt.Println("wunderground user: ", wundergroundUser)
@@ -149,7 +116,6 @@ func main() {
 	}
 
 	defer ftp.Close()
-	//fmt.Println("Successfully connected to", server)
 
 	// Username / password authentication
 	if err = ftp.Login(wundergroundUser, wundergroundPass); err != nil {
@@ -157,13 +123,7 @@ func main() {
 	}
 
 	// Upload a file
-	//var file *os.File
-	//if file, err = os.Open("/files/go/src/github.com/trodemaster/wxcamupload/image.jpg"); err != nil {
-	//	panic(err)
-	//}
-
 	if err := ftp.Stor("/image.jpg", buf); err != nil {
 		panic(err)
 	}
-
 }
